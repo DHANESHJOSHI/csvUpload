c434ececcfd12bb9d0767f6cd415a28654ae53da
DHANESHJOSHI <hydradhanesh@gmail.com>
2025-01-13 13:42:26 +0530

    ==>updated

--- src/models/Scholarship.js
+++ src/models/Scholarship.js
@@ -4,7 +4,6 @@ const ScholarshipSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     status: { type: String, required: true, default: 'pending' },
-
 });
 
 const Scholarship = mongoose.models.Scholarship || mongoose.model('Scholarship', ScholarshipSchema);
